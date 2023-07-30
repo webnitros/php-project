b4e52ec182c1cd066868608af3e60d90e40ece4e
Андрей <info@bustep.ru>
2023-07-30 14:29:32 +0700

    env

--- public/index.php
+++ public/index.php
@@ -6,4 +6,15 @@
  * Time: 23:38
  */
 
+define('BASE_DIR', dirname(__FILE__) . '/');
+
+require_once BASE_DIR . 'vendor/autoload.php';
+$appClass = 'modY';
+$envfile = BASE_DIR . '.env';
+
+\App\Helpers\Env::loadFile($envfile);
+
+
 echo 'Hello world!' . PHP_EOL;
+
+echo 'DB_USERNAME: ' . getenv('DB_USERNAME');
