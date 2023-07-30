f6f14106f23301a5625d71eb35a8abc532438338
Андрей <info@bustep.ru>
2023-07-30 16:16:34 +0700

    теперь запускаем

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * Time: 23:38
  */
 
-define('BASE_DIR', dirname(__FILE__,2) . '/');
+define('BASE_DIR', dirname(__FILE__, 2) . '/');
 
 require_once BASE_DIR . 'vendor/autoload.php';
 $envfile = BASE_DIR . '.env';
@@ -16,3 +16,35 @@ $envfile = BASE_DIR . '.env';
 echo 'Hello world!' . PHP_EOL;
 
 echo 'DB_USERNAME: ' . getenv('DB_USERNAME');
+
+
+echo 'Подключение к DB' . PHP_EOL;
+
+$servername = "mysql"; // имя сервиса MySQL в вашем стеке
+$username = "root"; // имя пользователя MySQL
+$password = "password"; // пароль пользователя MySQL
+$dbname = "database"; // имя базы данных MySQL
+
+// Создаем подключение
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверяем подключение
+if ($conn->connect_error) {
+    die("Ошибка подключения: " . $conn->connect_error);
+}
+
+// Выполняем запросы к базе данных
+$sql = "SELECT * FROM ara3_categories";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Обрабатываем результаты запроса
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . PHP_EOL;
+    }
+} else {
+    echo "0 результатов" . PHP_EOL;
+}
+
+// Закрываем подключение
+$conn->close();
